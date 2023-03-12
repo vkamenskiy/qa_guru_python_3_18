c7269ac9a3c78a3308aa07d56364ee54487a1d7c
Vladislav <djefry.kamen@gmail.com>
2023-03-12 22:27:23 +0400

    add test_demoshop

--- tests/test_demoshop.py
+++ tests/test_demoshop.py
@@ -1,87 +1,113 @@
 import os
-import time
-
-import requests
-from allure_commons._allure import step
+import allure
 from dotenv import load_dotenv
-from selene import have
+from selene import have, be
 from selene.support.shared import browser
-
-from utils.base_session import BaseSession
 
 load_dotenv()
 
 LOGIN = os.getenv("DEMOSHOP_LOGIN")
-PASSWORD = os.getenv("DEMOSHOP_PASSWORD")
-API_URL = os.getenv("DEMOSHOP_API_URL")
 WEB_URL = os.getenv("DEMOSHOP_WEB_URL")
 
 browser.config.base_url = WEB_URL
 
 
-def test_add_product_to_cart(demoshop):
-    response = demoshop.post(
-        "/login",
-        json={"Email": "qa_guru_3_14@gu.ru", "Password": "123456"},
-        allow_redirects=False,
-    )
-    authorization_cookie = response.cookies.get("NOPCOMMERCE.AUTH")
+def test_add_product_to_cart(demoshop_browser, demoshop):
+    with allure.step('Open start page'):
+        browser.open("")
 
-    browser.open("/Themes/DefaultClean/Content/images/logo.png")
+    with allure.step('Add product to cart'):
+        demoshop.post(
+            "/addproducttocart/details/72/1",
+            data={
+                "product_attribute_72_5_18": 53,
+                "product_attribute_72_6_19": 54,
+                "product_attribute_72_3_20": 57,
+                "addtocart_72.EnteredQuantity": 1,
+            }
+        )
 
-    browser.driver.add_cookie(
-        {"name": "NOPCOMMERCE.AUTH", "value": authorization_cookie}
-    )
-    browser.open("")
+    with allure.step('Check product in cart'):
+        browser.element(".header-links #topcartlink").click()
+        browser.element(".product-name").should(have.text('Build your own cheap computer'))
 
-    # browser.open("/register")
-    # browser.element("#FirstName").type('Ivan')
-    # browser.element("#LastName").type('Ivanov')
-    # browser.element('#Email').type('testivanivanovtest@test.ru')
-    # browser.element('#Password').type('123456')
-    # browser.element('#ConfirmPassword').type('123456')
-    # browser.element('#register-button').click()
-
-    # browser.open("/build-your-cheap-own-computer")
-    requests.post(
-        "https://demowebshop.tricentis.com/addproducttocart/details/72/1",
-        json={
-            "product_attribute_72_5_18": 53,
-            "product_attribute_72_6_19": 54,
-            "product_attribute_72_3_20": 57,
-            "addtocart_72.EnteredQuantity": 1,
-        }
-    )
-    browser.element(".header-links #topcartlink").click()
-    browser.element('.update-cart-button').click()
-    browser.element("[name=itemquantity3081838]").should(have.size(2))
+    with allure.step('Remove products from the cart'):
+        browser.element('.remove-from-cart > [name = "removefromcart"]').click()
+        browser.element('.update-cart-button').click()
+        browser.element('div.order-summary-content').should(have.text('Your Shopping Cart is empty!'))
 
 
+def test_delete_product_from_the_cart(demoshop_browser, demoshop):
+    with allure.step('Open start page'):
+        browser.open("")
 
-    # browser.open("/build-your-cheap-own-computer")
-    # browser.element(".header-links #topcartlink").click()
-    # browser.element('.update-cart-button').click()
-    # browser.element("[name=itemquantity3081621]").should(have.size(13))
-    # add_product_cookie = response.cookies.get("NOPCOMMERCE.AUTH")
-    #
-    # browser.driver.add_cookie({'name': 'NOPCOMMERCE.AUTH', 'value': add_product_cookie})
-    # browser.open('')
+    with allure.step('Add product to cart'):
+        demoshop.post(
+            "/addproducttocart/details/72/1",
+            data={
+                "product_attribute_72_5_18": 53,
+                "product_attribute_72_6_19": 54,
+                "product_attribute_72_3_20": 57,
+                "addtocart_72.EnteredQuantity": 1,
+            }
+        )
+
+    with allure.step('Delete product from the cart'):
+        browser.element(".header-links #topcartlink").click()
+        browser.element('.remove-from-cart > [name = "removefromcart"]').click()
+        browser.element('.update-cart-button').click()
+        browser.element(".order-summary-content").should(have.text('Your Shopping Cart is empty!'))
 
 
-# def test_login_though_api_with_base_session_fixture(demoshop):
-#     response = demoshop.post(
-#         "/login",
-#         json={"Email": "qa_guru_3_14@gu.ru", "Password": "123456"},
-#         allow_redirects=False,
-#     )
-#     authorization_cookie = response.cookies.get("NOPCOMMERCE.AUTH")
-#
-#     browser.open("/Themes/DefaultClean/Content/images/logo.png")
-#
-#     browser.driver.add_cookie(
-#         {"name": "NOPCOMMERCE.AUTH", "value": authorization_cookie}
-#     )
-#     browser.open("")
-#
-#     with step("Verify successful authorization"):
-#         browser.element(".account").should(have.text(LOGIN))
+def test_add_product_to_wishlist(demoshop_browser, demoshop):
+    with allure.step('Open start page'):
+        browser.open("")
+
+    with allure.step('Add product to wishlist'):
+        demoshop.post(
+            '/addproducttocart/details/43/2',
+            data={
+                'addtocart_43.EnteredQuantity': 1
+            }
+        )
+
+    with allure.step('Check product in wishlist'):
+        browser.element('.header-links .ico-wishlist').click()
+        browser.element('.cart-item-row .product > [href="/smartphone"]').should(have.text('Smartphone'))
+
+    with allure.step('Remove products from the wishlist'):
+        browser.element('.remove-from-cart > [name = "removefromcart"]').click()
+        browser.element('.update-wishlist-button').click()
+        browser.element('.wishlist-content').should(have.text('The wishlist is empty!'))
+
+
+def test_delete_product_from_the_wishlist(demoshop_browser, demoshop):
+    with allure.step('Open start page'):
+        browser.open("")
+
+    with allure.step('Add product to wishlist'):
+        demoshop.post(
+            '/addproducttocart/details/43/2',
+            data={
+                'addtocart_43.EnteredQuantity': 1
+            }
+        )
+
+    with allure.step('Delete product from the wishlist'):
+        browser.element('.header-links .ico-wishlist').click()
+        browser.element('.remove-from-cart > [name = "removefromcart"]').click()
+        browser.element('.update-wishlist-button').click()
+        browser.element('.wishlist-content').should(have.text('The wishlist is empty!'))
+
+
+def test_logout(demoshop_browser):
+    with allure.step('Open start page'):
+        browser.open("")
+
+    with allure.step('Verify successful authorization'):
+        browser.element(".account").should(have.text(LOGIN))
+
+    with allure.step('Logout'):
+        browser.element('.ico-logout').click()
+        browser.element('.ico-login').should(be.visible)
+
